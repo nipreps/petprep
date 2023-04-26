ccc586ec8e3dbe8b06a2ca9aaca438ba57e192d9
Martin Norgaard <martin.noergaard@nru.dk>
2023-04-26 14:49:57 +0200

    FIX: Update LoganModel and corresponding test

--- petprep/interfaces/kinmod.py
+++ petprep/interfaces/kinmod.py
@@ -1,50 +1,75 @@
 import numpy as np
-import pandas as pd
-from scipy.stats import linregress
+from scipy import integrate
+import statsmodels.api as sm
 from nipype.interfaces.base import BaseInterface, BaseInterfaceInputSpec, TraitedSpec, File, traits
 
 class LoganModelInputSpec(BaseInterfaceInputSpec):
+    frame_times = traits.Array(desc="Mid-time for each frame (in minutes)", mandatory=True)
     reference_tac = traits.Array(desc="Reference tissue TAC", mandatory=True)
-    target_tac = traits.Array(desc="Target tissue TAC", mandatory=True)
-    frame_times = traits.Array(desc="Frame times", mandatory=True)
-    start_time = traits.Float(desc="Start time", mandatory=True)
+    target_tac = traits.Array(desc="ROI tissue TAC", mandatory=True)
+    tstar = traits.Float(desc="Start time for linear regression", mandatory=True)
+    k2ref = traits.Float(desc="k2ref", mandatory=True)
+    weights = traits.Array(desc="Frame weights", mandatory=True)
 
 class LoganModelOutputSpec(TraitedSpec):
     Kappa2 = traits.Float(desc="Kappa2 value")
-    VT = traits.Float(desc="VT value")
+    BPnd = traits.Float(desc="BPnd value")
+    MSE = traits.Float(desc="Mean Squared Error")
+    FPE = traits.Float(desc="Final Prediction Error")
+    SigmaSqr = traits.Float(desc="Squared Standard Deviation of Prediction Error")
+    LogLike = traits.Float(desc="Log-likelihood")
+    AIC = traits.Float(desc="Akaike Information Criterion")
 
 class LoganModel(BaseInterface):
     input_spec = LoganModelInputSpec
     output_spec = LoganModelOutputSpec
 
-    @staticmethod
-    def cumtrapz_l(t, y):
-        return np.cumsum(y * np.diff(t, prepend=0))
-
     def _run_interface(self, runtime):
+        frame_times = self.inputs.frame_times
         reference_tac = self.inputs.reference_tac
         target_tac = self.inputs.target_tac
-        frame_times = self.inputs.frame_times
-        start_time = self.inputs.start_time
+        tstar = self.inputs.tstar
+        k2ref = self.inputs.k2ref
+        weights = self.inputs.weights
 
-        start_idx = np.argmax(frame_times >= start_time)
+        start_idx = np.where(frame_times > tstar)[0][0]
 
-        ref_integral = self.cumtrapz_l(frame_times, reference_tac)
-        tar_integral = self.cumtrapz_l(frame_times, target_tac)
+        cref_int = integrate.cumtrapz(reference_tac, frame_times, initial=0)
+        croi_int = integrate.cumtrapz(target_tac, frame_times, initial=0)
 
-        x = ref_integral[start_idx:] / target_tac[start_idx:]
-        y = tar_integral[start_idx:] / target_tac[start_idx:]
+        x = (cref_int + reference_tac / k2ref) / target_tac
+        y = croi_int / target_tac
 
-        # Linear regression
-        slope, intercept, r_value, p_value, std_err = linregress(x, y)
+        xfit = x[start_idx:]
+        yfit = y[start_idx:]
+        weights_fit = weights[start_idx:]
 
-        self._Kappa2 = -1 / intercept
-        self._VT = slope
+        xfit_add_constant = sm.add_constant(xfit)
+
+        glm = sm.GLM(yfit, xfit_add_constant, family=sm.families.Gaussian(), var_weights=weights_fit)
+
+        fitted_glm = glm.fit()
+
+        yhat = fitted_glm.fittedvalues
+
+        self._Kappa2 = -1 / fitted_glm.params[0]
+        self._BPnd = fitted_glm.params[1] - 1
+
+        self._MSE = np.sum((yhat - yfit) ** 2) / (len(yfit) - 3)
+        self._FPE = np.sum((yhat - yfit) ** 2) * (len(yfit) + 3) / (len(yfit) - 3)
+        self._SigmaSqr = np.std(yhat - yfit) ** 2
+        self._LogLike = -0.5 * len(yfit) * np.log(2 * np.pi * self._SigmaSqr) - 0.5 * np.sum((yhat - yfit) ** 2) / self._SigmaSqr
+        self._AIC = -2 * self._LogLike + 2 * 4
 
         return runtime
 
     def _list_outputs(self):
         outputs = self._outputs().get()
         outputs["Kappa2"] = self._Kappa2
-        outputs["VT"] = self._VT
+        outputs["BPnd"] = self._BPnd
+        outputs["MSE"] = self._MSE
+        outputs["FPE"] = self._FPE
+        outputs["SigmaSqr"] = self._SigmaSqr
+        outputs["LogLike"] = self._LogLike
+        outputs["AIC"] = self._AIC
         return outputs
--- petprep/interfaces/tests/test_logan_model.py
+++ petprep/interfaces/tests/test_logan_model.py
@@ -6,13 +6,17 @@ def test_logan_model():
     frame_times = np.array([0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300])
     reference_tac = np.array([0, 10, 20, 25, 28, 30, 31, 31.5, 32, 32.2, 32.3])
     target_tac = np.array([0, 5, 12, 17, 20, 21, 22, 22.5, 23, 23.2, 23.3])
-    start_time = 90
+    tstar = 90
+    k2ref = 0.05
+    weights = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
 
     logan_node = Node(LoganModel(), name="logan_model")
     logan_node.inputs.reference_tac = reference_tac
     logan_node.inputs.target_tac = target_tac
     logan_node.inputs.frame_times = frame_times
-    logan_node.inputs.start_time = start_time
+    logan_node.inputs.tstar = tstar
+    logan_node.inputs.k2ref = k2ref
+    logan_node.inputs.weights = weights
 
     wf = Workflow(name="logan_wf")
     wf.add_nodes([logan_node])
@@ -26,6 +30,17 @@ def test_logan_model():
             break
 
     assert results.Kappa2 is not None
-    assert results.VT is not None
+    assert results.BPnd is not None
+    assert results.MSE is not None
+    assert results.FPE is not None
+    assert results.SigmaSqr is not None
+    assert results.LogLike is not None
+    assert results.AIC is not None
+
     assert isinstance(results.Kappa2, float)
-    assert isinstance(results.VT, float)
+    assert isinstance(results.BPnd, float)
+    assert isinstance(results.MSE, float)
+    assert isinstance(results.FPE, float)
+    assert isinstance(results.SigmaSqr, float)
+    assert isinstance(results.LogLike, float)
+    assert isinstance(results.AIC, float)
