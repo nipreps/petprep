dbb8c14748da81a2722005bdc9be22fb1cd4b190
mnoergaard <martin.noergaard@nru.dk>
2025-06-18 14:18:25 +0200

    ENH: add stacking of tissue probability maps from a given list of files (tpms)

--- petprep/interfaces/pvc.py
+++ petprep/interfaces/pvc.py
@@ -5,7 +5,6 @@ from nipype.interfaces.base import (
     BaseInterfaceInputSpec,
     TraitedSpec,
     File,
-    traits,
 )
 import os
 
@@ -42,3 +41,47 @@ class Binarise4DSegmentation(BaseInterface):
 
     def _list_outputs(self):
         return {"out_file": os.path.abspath(self.inputs.out_file)}
+    
+
+class StackProbSegMapsInputSpec(BaseInterfaceInputSpec):
+    probseg_files = InputMultiPath(
+        File(exists=True),
+        mandatory=True,
+        desc="List of tissue probability segmentation maps (_probseg.nii.gz).",
+    )
+    out_file = File(
+        "stacked_probseg_maps.nii.gz",
+        usedefault=True,
+        desc="Output 4D stacked probability segmentation maps.",
+    )
+
+
+class StackProbSegMapsOutputSpec(TraitedSpec):
+    out_file = File(exists=True, desc="Output 4D stacked probability segmentation maps.")
+
+
+class StackProbSegMaps(BaseInterface):
+    input_spec = StackProbSegMapsInputSpec
+    output_spec = StackProbSegMapsOutputSpec
+
+    def _run_interface(self, runtime):
+        images = [nb.load(img_file) for img_file in self.inputs.probseg_files]
+
+        shapes = [img.shape for img in images]
+        affines = [img.affine for img in images]
+
+        if len(set(shapes)) > 1 or len(set([tuple(a.flatten()) for a in affines])) > 1:
+            raise ValueError("All probability maps must have the same shape and affine.")
+
+        stacked_data = np.stack([img.get_fdata() for img in images], axis=-1)
+
+        stacked_img = nb.Nifti1Image(
+            stacked_data, affine=images[0].affine, header=images[0].header
+        )
+        nb.save(stacked_img, os.path.abspath(self.inputs.out_file))
+
+        return runtime
+
+    def _list_outputs(self):
+        return {"out_file": os.path.abspath(self.inputs.out_file)}
+
