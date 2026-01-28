f1148e776a959b58500aa75a0a3b279da0ab16d3
mnoergaard <martin.noergaard@nru.dk>
2026-01-28 10:08:59 +0100

    Revert "FIX: style"
    This reverts commit 04e9eee961037ab64c58c4d1fd133201a13c3348.

--- petprep/interfaces/reports.py
+++ petprep/interfaces/reports.py
@@ -35,6 +35,7 @@ from uuid import uuid4
 import nibabel as nb
 import numpy as np
 import svgutils.transform as svgt
+from svgutils.compose import Unit
 from nilearn import image as nlimage
 from nilearn.plotting import plot_anat
 from nipype.interfaces.base import (
@@ -51,7 +52,6 @@ from nipype.interfaces.base import (
 from nireports.reportlets.utils import cuts_from_bbox, extract_svg, robust_set_limits
 from nireports.tools.ndimage import rotate_affine, rotation2canonical
 from smriprep.interfaces.freesurfer import ReconAll
-from svgutils.compose import Unit
 
 LOGGER = logging.getLogger('nipype.interface')
 
