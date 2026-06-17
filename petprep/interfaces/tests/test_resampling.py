27ac758b0f7d0c1304e38de9f33cfde18f55ddb0
mnoergaard <martin.noergaard@nru.dk>
2026-06-17 23:41:00 +0200

    TST: Cover 3D resample_series path

--- petprep/interfaces/tests/test_resampling.py
+++ petprep/interfaces/tests/test_resampling.py
@@ -4,6 +4,24 @@ import pytest
 from ..resampling import resample_series
 
 
+def test_resample_series_handles_3d_input():
+    """3D inputs should be resampled as a single volume."""
+
+    data = np.arange(8, dtype=np.float32).reshape((2, 2, 2))
+    coordinates = np.indices(data.shape, dtype=np.float32)
+
+    resampled = resample_series(
+        data,
+        coordinates,
+        [np.eye(4)],
+        output_dtype=np.float32,
+        order=0,
+    )
+
+    assert resampled.shape == data.shape
+    assert np.allclose(resampled, data)
+
+
 def test_resample_series_rejects_mismatched_hmc_transforms():
     """HMC transform mappings must contain one affine per PET volume."""
 
